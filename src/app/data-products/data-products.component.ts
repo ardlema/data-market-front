7e8bc039e9b27ba8aaf8d5219ee71b182e2e817e
Alberto Rodríguez <ardlema@gmail.com>
2021-02-08 12:45:24 +0100

    List of data products

--- src/app/data-products/data-products.component.ts
+++ src/app/data-products/data-products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataProduct } from '../data-product';
+import { DATAPRODUCTS } from '../mock-data-products';
 
 @Component({
   selector: 'app-data-products',
@@ -12,6 +13,13 @@ export class DataProductsComponent implements OnInit {
     id: 1,
     name: 'Post Flight Report ingester'
   };
+
+  dataProducts = DATAPRODUCTS;
+
+  selectedDataProduct: DataProduct;
+  onSelect(dataProduct: DataProduct): void {
+    this.selectedDataProduct = dataProduct;
+  }
 
   constructor() { }
 
