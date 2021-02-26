5676da4d86642be75cebcbc4ac85d74bae88d428
Alberto Rodríguez <ardlema@gmail.com>
2021-02-26 12:29:38 +0100

    Add 2 new products and get rid of the upper navigation buttons

--- src/app/data-product.ts
+++ src/app/data-product.ts
@@ -7,6 +7,15 @@ export interface DataProduct {
   input_port: Array<InputPort>;
   output_port: Array<OutputPort>;
   image: string;
+  description: string;
+  owner_name: string;
+  owner_email: string;
+  domain_context: string;
+  data_classification_level: string;
+  creation_date: string;
+  latest_update: string;
+  observability_reports_link: string;
+  data_reliability: string;
   rate: string;
 }
 
